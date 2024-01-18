df160448a694c73de41fe6b61b6ba351662af65d
Swarup Ripponpete Muralidhara <sripponp@synamedia.com>
2024-01-18 11:36:48 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import Location from './location/Locations';
-import DateTime from './date-and-time/DateTime';
-import imageSrc from './assets/design.png';
+import Home from './home/Home';
+import myLogo from './assets/myLogo.jpeg';
 
 function App() {
-  return (
-    <div className="app_root">
-      <div className="margin">
-        <div className="background-container">
-          <div style={{ color: '#fff', display: 'flex', justifyContent: 'center', margin: '40px', marginTop: 'auto' }}>||श्री:||</div>
-          <div className="level1"><b>Chethan S D & Saniha R Jois</b></div>
-          <div className="level2">Inviting you to the</div>
-          {/* <div className='nameHeader'> */}
-          {/* <div className="level3">Naming Ceremony</div> */}
-          {/* </div> */}
+  const [showHomePage, setShowHomePage] = useState(false);
 
-          <div className='level3'>
-            <img src={imageSrc} height="155px" width="200px"/>
-          </div>
-          
-          <div className="level4">of their daughter</div>
-          <div className="level5">Seeking your Blessings</div>
-          <div className="level6">On</div>
-          <div className="level8">
-            <DateTime />
-          </div>
-          <div className="level7">At</div>
-          <div className="level8">
-            <Location />
-          </div>
-          <div>
-            <p>
-              <span className='signTitle'>Best Compliments from <br /></span>
-              <span className='signContent'>Jois & Seethur Family</span>
-            </p>
-          </div>
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowHomePage(true);
+    }, 3000);
+
+    // Clear the timer when the component unmounts or when the 5 seconds are over
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div>
+      {showHomePage ? (
+        <Home />
+      ) : (
+        <div className='rootPage'>
+         {/* <h1> <p className='welcomeText'>Welcoming<br />
+         you to the <br />
+         Naming Ceremony</p></h1> */}
+         <img src={myLogo} width='350px' height='350px'
+         style={{
+          animation: 'blink 2s',
+         }}/>
         </div>
-      </div>
+      )}
     </div>
   );
 }
